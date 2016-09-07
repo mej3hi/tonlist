4e1312991ec06a4db7859a41c77d459c2bc4dc86
martin <martin@martin-Aspire-E1-572G>
2016-09-07 11:36:07 +0000

    upfardi index

--- src/main/java/com/tonlist/controller/IndexController.java
+++ src/main/java/com/tonlist/controller/IndexController.java
@@ -1,13 +1,17 @@
 package com.tonlist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 	
-    @RequestMapping("/")
-    String index(){
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    String index(ModelMap model){
+		model.put("serverTime", "Þetta tokst");
+		
         return "index";
     }
  
