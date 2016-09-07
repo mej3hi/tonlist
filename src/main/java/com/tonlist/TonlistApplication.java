ebf6161e8b060eee2d69c304bcb7651a8fded473
mej3hi <mej3@hi.is>
2016-09-07 00:36:51 +0000

    Add files via upload

---
+++ src/main/java/com/tonlist/TonlistApplication.java
@@ -0,0 +1,12 @@
+package com.tonlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonlistApplication.class, args);
+	}
+}
---
+++ src/main/java/com/tonlist/support/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.tonlist.support;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ThymeleafLayoutInterceptor());
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations(
+                    "classpath:/META-INF/resources/webjars/");
+        }
+        if (!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(
+            		"classpath:/");
+        }
+    }
+   
+}
