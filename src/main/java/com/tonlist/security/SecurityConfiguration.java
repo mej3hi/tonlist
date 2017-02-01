9259ed12107df484a2093f567ef25dc80c41e0fb
martin <martin@martin-Aspire-E1-572G>
2017-02-01 23:26:32 +0000

    Breyti scurity fyrir appið og bætti indexMobController

--- src/main/java/com/tonlist/security/SecurityConfiguration.java
+++ src/main/java/com/tonlist/security/SecurityConfiguration.java
@@ -1,14 +1,18 @@
 package com.tonlist.security;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 
 /**
@@ -18,6 +22,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, proxyTargetClass = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	
@@ -30,24 +35,63 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }    
 	
   
-    @Override
+   /* @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity 
+        httpSecurity.csrf().disable() 
         	.authorizeRequests()
         		.antMatchers("/","/calander", "/signUp","/forgetPassword","/resetPassword","/favicon.ico","/static/**","/webjars/**").permitAll()
         		.anyRequest().authenticated()
         		.and()
     		.formLogin()
-    			.loginPage("/signIn")
-    			.defaultSuccessUrl("/",true)
+    			.loginPage("/m/signIn")
+    			.defaultSuccessUrl("/m/",true)
     			.permitAll()
     			.and()
 			.logout()
 				.permitAll();
-				
-     
+		
+  
         
-    }
+    }*/
+    
+	    @Configuration
+	    @Order(1)
+	    public static class ApiWebSecurityConfig extends WebSecurityConfigurerAdapter{
+	    	@Override
+	    	protected void configure(HttpSecurity http) throws Exception {
+	            http
+                .antMatcher("/m/**")
+                .authorizeRequests()
+                	.antMatchers("/m/").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .httpBasic();	
+	    	}
+	    	
+	    }
+	    
+	    @Configuration
+	    @Order(2)
+	    public static class FormWebSecurityConfig extends WebSecurityConfigurerAdapter{
+	    	@Override
+	    	protected void configure(HttpSecurity http) throws Exception {
+	            http
+	        	.authorizeRequests()
+	        		.antMatchers("/","/calander", "/signUp","/forgetPassword","/resetPassword","/favicon.ico","/static/**","/webjars/**").permitAll()
+	        		.anyRequest().authenticated()
+	        		.and()
+	    		.formLogin()
+	    			.loginPage("/signIn")
+	    			.defaultSuccessUrl("/",false)
+	    			.permitAll()
+	    			.and()
+				.logout()
+					.permitAll();
+	    	
+	    	}	
+	    }
+    
+    
     
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
